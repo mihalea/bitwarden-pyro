d30ec7914212627bb103aa13c16ccba93be27b20
Mircea Mihalea <mircea@mihalea.ro>
2019-12-04 16:35:28 +0400

    Add source tag auto set

--- packaging/test.sh
+++ packaging/test.sh
@@ -4,6 +4,7 @@
 echo "MAKEPKG_DIR=$MAKEPKG_DIR"
 
 # Package version 
+tag=$(git describe --long | sed -rn 's/^(.*)-.*-.*$/\1/p')
 version=$(git describe --long | sed 's/\([^-]*-g\)/r\1/;s/-/./g')
 echo "version=${version}"
 
@@ -12,7 +13,8 @@ git clone ssh://aur@aur.archlinux.org/bitwarden-pyro-git.git aur
 cd aur
 
 # Change pkgver to current version
-sed -ir "s/^(pkgver)=(.*)$/\1=${version}/g" PKGBUILD
+sed -i -r "s/^(pkgver)=(.*)$/\1=${version}/g" PKGBUILD
+sed -i -r "s/^(source=.*#tag)=(.*)$/\1=${tag}\")/g" PKGBUILD
 
 # Create SRC info
 /bin/bash "$MAKEPKG_DIR/makepkg" --config="${MAKEPKG_CONF}" --printsrcinfo > test_src
